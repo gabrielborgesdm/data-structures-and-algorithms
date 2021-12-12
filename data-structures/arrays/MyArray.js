0c2c0a395340f376c94ea5cea5327dfaf9d48b53
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2021-12-12 10:23:30 -0300

    Add Array data structure

---
+++ data-structures/arrays/MyArray.js
@@ -0,0 +1,38 @@
+class MyArray {
+    constructor() {
+        this.length = 0
+        this.data = {}
+    }
+
+    get(index) {
+        return this.data[index]
+    }
+
+    push(item) {
+        this.data[this.length] = item
+        return ++this.length
+    }
+
+    pop() {
+        if (this.length === 0) return null
+        const lastItem = this.data[this.length - 1]
+        delete this.data[this.length - 1]
+        this.length--
+        return lastItem
+    }
+
+    delete(index) {
+        const deletedItem = this.data[index]
+        this.shiftItems(index)
+        return deletedItem
+    }
+
+    shiftItems(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1]
+        }
+        delete this.data[this.length - 1]
+        this.length--
+    }
+}
+
